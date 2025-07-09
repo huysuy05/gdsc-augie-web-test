5653f9bf4a1e302557c1105c673cb2722b2074cb
Hieu Nguyen <davidthehieu@gmail.com>
2025-07-09 17:38:35 -0500

    Initialize the responde model for the workshops CRUD

---
+++ backend-server/repositories/workshops.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+
+
+
+
+# A GET method to fetch all workshops from the db
+def get_all(db:Session):
+    # workshops = 
+    pass
+
+#A GET method to fetch single workshop from the db
+def get_single(db: Session):
+    pass
+
+# A POST method to create a new workshop
+def add_post(db:Session):
+    pass
+
+# A PUT Method to update an existing workshop
+def update_post(db: Session):
+    pass
+
+# A DELETE Method to delete a single workshop
+def delete(db: Session):
+    pass
--- backend-server/routes/workshops/workshops.py
+++ backend-server/routes/workshops/workshops.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from .. import schemas, database, oauth2
 from typing import List
+from repositories import workshops
+from sqlalchemy.orm import Session
 
 
 route = APIRouter(
@@ -10,5 +12,11 @@ route = APIRouter(
 
 
 @route.get("/", response_model=List[schemas.showWorkshops])
-def fetch_all_workshops():
-    return "Fetching"
+def fetch_all_workshops(db: Session):
+    return workshops.get_all(db)
+
+@route.get("/{id}", status_code=status.HTTP_200_OK)
+def fetch_single_workshop(db:Session):
+    return workshops.get_single(db)
+
+
