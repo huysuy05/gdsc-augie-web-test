af3ffe0e980ee9db161f750167d00a7267b0f240
Hieu Nguyen <davidthehieu@gmail.com>
2025-07-10 00:25:52 -0500

    Added some stuff to the response model

--- backend-server/schemas.py
+++ backend-server/schemas.py
@@ -2,10 +2,13 @@
 from pydantic import BaseModel
 # Import List type hint for defining list fields
 from typing import List
+from sqlalchemy import DateTime
 
 
 class WorkShops(BaseModel):
     title: str
     description: str
+    date: DateTime
+    
 
     
