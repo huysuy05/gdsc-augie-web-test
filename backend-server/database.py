7c7c0d947494f330b9dadefb5cb93818a58be9ca
Hieu Nguyen <hieunguyen23@augustana.edu>
2025-07-10 20:25:23 -0500

    Added comments to the db file

--- backend-server/database.py
+++ backend-server/database.py
@@ -9,15 +9,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-############ DEFINE AN URL FOR THE SQLITE DB ##################
-# This creates/connects to the Supabase database.
+
+######## THIS IS THE DATABASE URL FOR THE SUPABASE DB #########
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") #Connect to the workshop database
 
+###### Catch any erros if there is something wrong with the db url
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("DATABASE_URL env variables is not yet set")
 
 
-###### This creates/connects to a file named 'gdg.db' in the current directory ######
+###### THIS CREATES/CONNECTS A DATABASE ENGINE ######
 ### connect_args{...} allows multiple threads to open a connection with the db
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
