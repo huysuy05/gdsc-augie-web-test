ec077b3be42fb23060a4f02920346e898c0d0c8d
Huy Nguyen <huynguyen23@augustana.edu>
2025-07-12 14:33:03 -0500

    Added unit testings for hashing passwords

---
+++ backend-server/tests/test_hashing.py
@@ -0,0 +1,13 @@
+from hashing import Hash
+import pytest
+
+def test_bcrypt_hash_and_verify():
+    password = "testingpassword"
+    hashed_pw = Hash.bcrypt(password)
+
+    # Assure hashed password is not the same as orginal password
+    assert hashed_pw != password
+
+    # Hashed password should be verified with original password
+    assert Hash.verify(hashed_pw, password)
+    assert not Hash.verify(hashed_pw, "wrongpassword")
