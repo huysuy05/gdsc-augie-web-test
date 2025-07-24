3ebb987a9e1a57d4a77bdeb0247515c325f056a1
Hieu Nguyen <davidthehieu@gmail.com>
2025-07-24 16:13:13 -0500

    Updated the view single workshop

--- app/workshops/[id]/page.tsx
+++ app/workshops/[id]/page.tsx
@@ -1,7 +1,12 @@
+interface SingleWorkshopProps {
+    params: {
+        id: string;
+    };
+}
 
-
-export default async function ViewWorkshop (){
-    const URL = "http://127.0.0.1:8000/workshops/2"
+export default async function ViewWorkshop ({params}: SingleWorkshopProps){
+    const { id } = params;
+    const URL = "http://127.0.0.1:8000/workshops/" + id;
     const res = await fetch(URL)
     if (!res) {
         console.error("Cannot fetch a single workshop")
@@ -12,6 +17,7 @@ export default async function ViewWorkshop (){
         <div className="flex justify-center items-center">
             <h1>{data.title}</h1>
             <h2>{data.description}</h2>
+            <h2>{data.date}</h2>
         </div>
     )
-}
+}   
